b1edad5c3e114e2ef8c40a923cafaf31f2b7db3f
maher <gomemahero.com>
2022-08-08 14:43:27 +0300

    connecting socket io with nextjjs start work good

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,52 @@
 import React from 'react';
+import axios from 'axios'
+import { useState ,useEffect, useRef } from 'react'
+import io from "socket.io-client";
 
+let socket;
 const Login = () => {
+    const socketRef = useRef();
+    const ENDPOINT = 'https://node-heroku9393.herokuapp.com'
+
+    useEffect(() => {
+
+        // heroku https://node-heroku9393.herokuapp.com/
+// vercel  https://nodejs-vercel-nine.vercel.app
+// netlify   https://genuine-granita-abb198.netlify.app//.netlify/functions/api
+        axios.get('https://node-heroku9393.herokuapp.com/').then(res => {
+
+        console.log(res?.data);
+        
+
+    })
+
+    }, []);
+
+
+
+    useEffect(() => {
+      
+        socket = io(ENDPOINT ,   { transports: ['websocket', 'polling', 'flashsocket'] });
+        console.log('------->',socket);
+    
+      
+    
+     
+    
+        socketRef.current = io.connect('https://node-heroku9393.herokuapp.com/');
+    
+        socket.on("your id", id => {
+          console.log('your id',id);
+        }, [])
+    
+        socket.on("message", (message) => {
+         console.log('message',message);
+        })
+      }, []);
+
+
+
+
     return (
         <div>
             <h1>Login</h1>
